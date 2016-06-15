ded3607605f9321b121ae15ad96e9a676a093482
Joel A. Jaime <joel.alexander.jaime@gmail.com>
2016-06-15 15:58:53 -0400

    Agregando: varios metodos para obtener datos de Slack

--- slack.php
+++ slack.php
@@ -29,5 +29,55 @@ class Slack{
 		$this->slack_data 	= ['token' => 'xoxp-39152444724-39167426673-41774226577-e7528d9d52'];
 	}
 
+	/**
+	 * Metodo para generar una nueva consulta
+	 * @param $method  	Tipo de metodo que usaremos (disponibles en $slack_methods)
+	 * @param $type  	Tipo de consulta que se realizara (GET, POST, PUT, etc)
+	 * @author Joel A. Jaime <joel.alexander.jaime@gmail.com> (https://posicioncreativa.com) */
+	public function request($method, $type = 'POST'){
+		// Validando si el parametro es válido
+		if( in_array($method, $this->slack_methods) ){
+			// Generamos los datos a pasar por la URL
+			$parameters = http_build_query( $this->slack_data );
+			// Unimos la URL con el token
+			$request 	= implode( '?',[
+				$this->slack_url . $method , $parameters
+			]);
+			// Generamos el request
+			$response = $this->client->request($type, $request);
+			// Retornamos cualquier respuesta
+			return $response->getBody()->getContents();
+		}
+	}
+
+	/**
+	 * Metodo para obtener una lista con todos los usuarios del slack
+	 * @author Joel A. Jaime <joel.alexander.jaime@gmail.com> (https://posicioncreativa.com) */
+	public function get_users(){
+		return $this->request('users.list', 'GET');
+	}
+
+	/**
+	 * Metodo para obtener una lista con todos los canales disponibles
+	 * @author Joel A. Jaime <joel.alexander.jaime@gmail.com> (https://posicioncreativa.com) */
+	public function get_channels(){
+		return $this->request('channels.list', 'GET');	
+	}
+
+	/**
+	 * Metodo para enviar un mensaje a un canal por defecto
+	 * @param $username  	Nombre de Usuario que usara en el chat
+	 * @param $message  	Mensaje que el usuario quiere enviar al canal
+	 * @author Joel A. Jaime <joel.alexander.jaime@gmail.com> (https://posicioncreativa.com) */
+	public function send_message($username, $message){
+		// Colocamos el canal público por defecto
+		$this->slack_data['channel'] 	= $this->slack_channel;
+		// Ingresamos el mensaje que deseamos enviar
+		$this->slack_data['text']	 	= $message;
+		// Asignamos un usuario inviato
+		$this->slack_data['username']	= implode(' ', ['Invitado', '-', $username]);
+		// Publicamos el mensaje en Slack!
+		return $this->request('chat.postMessage', 'GET');
+	}
 	
 }
